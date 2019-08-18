a99f2c6a479a6ecb93b10217ba525a07bd2c373f
ambarkumar <ambarkumar85@gmail.com>
2019-08-18 11:01:59 +0530

    home page and story feed  page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { StoryComponent } from './story/story.component';
 
 const routes: Routes = [
 
   {
     path: 'home', component: HomeComponent,
+  
+},
+
+  {
+    path: 'story', component: StoryComponent,
   
 },
   {
